6771c13693f66f3669b39c30114b8386d2c16d36
Jeaven Onyango <jeaveno123@gmail.com>
2025-11-30 13:43:59 +0300

    Add Dog and Person classes with __init__ and instance methods

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+
+fido = Dog("Fido")
+print(fido.name)   # Fido
+print(fido.breed)  # Mutt
+fido.bark()        # Woof!
+fido.sit()         # The dog is sitting.
+fido.get_adopted("Sophie")
+print(fido.owner)  # Sophie
+
+snoopy = Dog("Snoopy", "Beagle")
+print(snoopy.name)  # Snoopy
+print(snoopy.breed) # Beagle
